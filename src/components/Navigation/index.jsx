97882884ba155f5584668425e3082da17f6bf623
Adrian Darian <adarian@ucmerced.edu>
2019-03-05 02:02:39 -0800

    sign out with firebase

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => {
@@ -21,6 +22,9 @@ const Navigation = () => {
       </li>
       <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+      <li>
+        <SignOutButton />
       </li>
     </ul>
   </div>
