cc7c425bde6eb38b9b85867ce8c3115e08f25b80
Adrian Darian <adarian@ucmerced.edu>
2019-03-05 12:42:42 -0800

    password reset

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -4,6 +4,7 @@ import { withRouter } from "react-router-dom";
 import { compose } from "recompose";
 
 import { SignUpLink } from "../SignUp";
+import {PasswordForgetLink} from '../PasswordForget';
 import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
@@ -11,6 +12,7 @@ const SignInPage = () => {
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>;
 };
