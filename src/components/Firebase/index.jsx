6b887fe393aab535bf7228f947d872a511e20ec8
Adrian Darian <adarian@ucmerced.edu>
2019-03-05 01:29:29 -0800

    firebase auth

--- src/components/Firebase/index.jsx
+++ src/components/Firebase/index.jsx
@@ -1,10 +1,7 @@
 /* eslint-disable no-unused-expressions */
-import React from 'react';
-
-const Firebase = () => {
-  <div>
-    <h1>Firebase</h1>
-  </div>
-};
+import FirebaseContext from './context';
+import Firebase from './firebase';
 
 export default Firebase;
+
+export { FirebaseContext };
