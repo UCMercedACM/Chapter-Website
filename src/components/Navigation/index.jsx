ba309a7f5b31000d121950b29f8bb8c5d20b40ef
Adrian Darian <adarian@ucmerced.edu>
2019-03-05 11:07:56 -0800

    session handling

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,16 +1,22 @@
 /* eslint-disable no-unused-expressions */
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import SignOutButton from '../SignOut';
-import * as ROUTES from '../../constants/routes';
+import SignOutButton from "../SignOut";
+import * as ROUTES from "../../constants/routes";
+import { AuthUserContext } from "../Session";
 
-const Navigation = () => {
+const Navigation = () => (
+  <div>
+    <AuthUserContext.Consumer>
+      {authUser => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
+    </AuthUserContext.Consumer>
+  </div>
+);
+
+const NavigationAuth = () => {
   <div>
     <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
       <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
       </li>
@@ -21,13 +27,23 @@ const Navigation = () => {
         <Link to={ROUTES.ACCOUNT}>Account</Link>
       </li>
       <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-      <li>
         <SignOutButton />
       </li>
     </ul>
-  </div>
+  </div>;
+};
+
+const NavigationNonAuth = () => {
+  <div>
+    <ul>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default Navigation;
--- src/components/Session/index.jsx
+++ src/components/Session/index.jsx
@@ -1,10 +1,5 @@
 /* eslint-disable no-unused-expressions */
-import React from 'react';
+import AuthUserContext from './context';
+import withAuthentication from './withAuthentication';
 
-const Session = () => {
-  <div>
-    <h1>Session</h1>
-  </div>
-};
-
-export default Session;
+export {AuthUserContext, withAuthentication};
