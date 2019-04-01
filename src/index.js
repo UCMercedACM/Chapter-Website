6443b683f81996ebbf7782b8df56fe81c6a4bc92
Adrian Darian <adarian@ucmerced.edu>
2019-03-31 23:12:35 -0700

    temp live site

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
+import "./assets/css/index.css";
 import * as serviceWorker from "./serviceWorker";
 
 import App from "./components/App";
-import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
-  <FirebaseContext.Provider value={new Firebase()}>
-    <App />
-  </FirebaseContext.Provider>,
+  <App />,
   document.getElementById("root")
 );
 
