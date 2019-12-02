7c27ed992cb2f799b113f428045ece0d83933133
Adrian Darian <adarian@ucmerced.edu>
2019-12-01 17:12:52 -0800

    ngrx 8

--- src/app/root-store/member-store/member-store.effects.ts
+++ src/app/root-store/member-store/member-store.effects.ts
@@ -3,15 +3,25 @@ import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { catchError, map, concatMap } from "rxjs/operators";
 import { EMPTY, of } from "rxjs";
 
+// import { DataService } from "../../services/data.service";
 import * as MemberStoreActions from "./member-store.actions";
 
 @Injectable()
 export class MemberStoreEffects {
+  // constructor(private dataService: DataService, private actions$: Actions) {}
+  constructor(private actions$: Actions) {}
+
   loadMemberStores$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(MemberStoreActions.loadMemberStores),
       concatMap(() =>
         /** An EMPTY observable only emits completion. Replace with your own observable API request */
+        // this.dataService.getItems().pipe(
+        //   map(data => MemberStoreActions.loadMemberStoresSuccess({ data })),
+        //   catchError(error =>
+        //     of(MemberStoreActions.loadMemberStoresFailure({ error }))
+        //   )
+        // )
         EMPTY.pipe(
           map(data => MemberStoreActions.loadMemberStoresSuccess({ data })),
           catchError(error =>
@@ -21,6 +31,4 @@ export class MemberStoreEffects {
       )
     );
   });
-
-  constructor(private actions$: Actions) {}
 }
--- src/app/root-store/root-store.module.ts
+++ src/app/root-store/root-store.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
 
-import * as fromWorkshopStore from './workshop-store/workshop-store.reducer';
-import { WorkshopStoreEffects } from './workshop-store/workshop-store.effects';
-import * as fromProjectStore from './project-store/project-store.reducer';
-import { ProjectStoreEffects } from './project-store/project-store.effects';
-import * as fromMemberStore from './member-store/member-store.reducer';
-import { MemberStoreEffects } from './member-store/member-store.effects';
+import * as fromWorkshopStore from "./workshop-store/workshop-store.reducer";
+import { WorkshopStoreEffects } from "./workshop-store/workshop-store.effects";
+import * as fromProjectStore from "./project-store/project-store.reducer";
+import { ProjectStoreEffects } from "./project-store/project-store.effects";
+import * as fromMemberStore from "./member-store/member-store.reducer";
+import { MemberStoreEffects } from "./member-store/member-store.effects";
 
 @NgModule({
   declarations: [],
@@ -17,7 +17,7 @@ import { MemberStoreEffects } from './member-store/member-store.effects';
     EffectsModule.forFeature([
       WorkshopStoreEffects,
       ProjectStoreEffects,
-      MemberStoreEffects,
+      MemberStoreEffects
     ]),
     StoreModule.forFeature(
       fromWorkshopStore.workshopStoreFeatureKey,
@@ -30,7 +30,7 @@ import { MemberStoreEffects } from './member-store/member-store.effects';
     StoreModule.forFeature(
       fromMemberStore.memberStoreFeatureKey,
       fromMemberStore.reducer
-    ),
+    )
   ]
 })
 export class RootStoreModule {}
