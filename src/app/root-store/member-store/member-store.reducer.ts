23e839eace8efb9712e65902a5f5f685e5f43602
Adrian Darian <adarian@ucmerced.edu>
2019-12-01 16:56:30 -0800

    updated member state and selectors

--- src/app/root-store/member-store/member-store.reducer.ts
+++ src/app/root-store/member-store/member-store.reducer.ts
@@ -1,37 +1,9 @@
-import { Action, createReducer, on, State } from '@ngrx/store';
-import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import { Action, createReducer, on } from "@ngrx/store";
 
-import * as MemberStoreActions from './member-store.actions';
-import { Member } from '../../models/member';
+import { MemberState, initialState, memberStoreAdapter } from "./member-store.state";
+import * as MemberStoreActions from "./member-store.actions";
 
-export const memberStoreFeatureKey = 'memberStore';
-
-export interface MemberState extends EntityState<Member> {
-  // additional entities state properties
-  selectedMemberId: number;
-  isLoading?: boolean;
-  error?: any;
-}
-
-export function selectedMemberId(a: Member): string {
-  //In this case this would be optional since primary key is id
-  return a.id;
-}
-
-export function sortByName(a: Member, b: Member): number {
-  return a.firstName.localeCompare(b.firstName);
-}
-
-export const memberStoreAdapter: EntityAdapter<Member> = createEntityAdapter<Member>({
-  selectId: selectedMemberId,
-  sortComparer: sortByName,
-});
-
-export const initialState: MemberState = memberStoreAdapter.getInitialState({
-  selectedMemberId: null,
-  isLoading: false,
-  error: null
-});
+export const memberStoreFeatureKey = "memberStore";
 
 const memberStoreReducer = createReducer(
   initialState,
--- src/app/root-store/member-store/member-store.selectors.ts
+++ src/app/root-store/member-store/member-store.selectors.ts
@@ -1,6 +1,41 @@
-import { createFeatureSelector, createSelector } from "@ngrx/store";
-import * as fromMemberStore from "./member-store.reducer";
+import {
+  createFeatureSelector,
+  createSelector,
+  MemoizedSelector
+} from "@ngrx/store";
 
-export const selectMemberStoreState = createFeatureSelector<
-  fromMemberStore.State
->(fromMemberStore.memberStoreFeatureKey);
+import { Member } from "../../models/member";
+
+import { memberStoreAdapter, MemberState } from "./member-store.state";
+
+export const getError = (state: MemberState): any => state.error;
+
+export const getIsLoading = (state: MemberState): boolean => state.isLoading;
+
+export const selectMemberState: MemoizedSelector<
+  object,
+  MemberState
+> = createFeatureSelector<MemberState>("member");
+
+export const selectAllMemberItems: (
+  state: object
+) => Member[] = memberStoreAdapter.getSelectors(selectMemberState).selectAll;
+
+export const selectMemberById = (id: string) =>
+  createSelector(this.selectAllMemberItems, (allMembers: Member[]) => {
+    if (allMembers) {
+      return allMembers.find(p => p.id === id);
+    } else {
+      return null;
+    }
+  });
+
+export const selectMemberError: MemoizedSelector<
+  object,
+  any
+> = createSelector(selectMemberState, getError);
+
+export const selectMemberIsLoading: MemoizedSelector<
+  object,
+  boolean
+> = createSelector(selectMemberState, getIsLoading);
