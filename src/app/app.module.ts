16e5e1b13492bbf649a29e28b7cfd0af7fe83a5f
Adrian Darian <adarian@ucmerced.edu>
2019-12-09 17:18:13 -0800

    reduxdevtools rendering data now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -17,6 +18,7 @@ import { CodeEditorComponent } from "./containers/code-editor/code-editor.compon
 import { InterviewsComponent } from "./containers/interviews/interviews.component";
 import { CalendarComponent } from "./containers/calendar/calendar.component";
 import { RootStoreModule } from "./root-store";
+import { environment } from "../environments/environment"; // Angular CLI environment
 
 @NgModule({
   declarations: [
@@ -34,7 +36,17 @@ import { RootStoreModule } from "./root-store";
     InterviewsComponent,
     CalendarComponent
   ],
-  imports: [HttpClientModule, BrowserModule, AppRoutingModule, RootStoreModule],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    RootStoreModule,
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
