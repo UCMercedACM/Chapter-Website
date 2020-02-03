a539223d694471e1e661015fea67e35968d4a5f0
Bob <robertperrine@icloud.com>
2020-02-02 21:03:32 -0800

    Added Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -34,7 +35,7 @@ import { RootStoreModule } from "./root-store";
     InterviewsComponent,
     CalendarComponent
   ],
-  imports: [HttpClientModule, BrowserModule, AppRoutingModule, RootStoreModule],
+  imports: [HttpClientModule, BrowserModule, AppRoutingModule, RootStoreModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
