31b051b86cd8584a0bde02d0a36ecd781932865c
Adrian Darian <adarian@ucmerced.edu>
2020-03-19 22:09:45 -0700

    Footer complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
+import { faTwitter, faLinkedinIn, faFacebookF, faGithub, faDiscord, faSlack, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { } from '@fortawesome/free-solid-svg-icons';
 
 import { MemberStoreActions } from "./root-store";
 
@@ -12,6 +15,14 @@ export class AppComponent implements OnInit {
   navbarExtend: boolean = false;
   professionalOpen: boolean = true;
   profileOpen: boolean = true;
+  faTwitter = faTwitter;
+  faLinkedinIn = faLinkedinIn;
+  faFacebookF = faFacebookF;
+  faGithub = faGithub;
+  faDiscord = faDiscord;
+  faSlack = faSlack;
+  faCopyright = faCopyright;
+  faInstagram = faInstagram;
 
   constructor(private store: Store<{}>) {}
 
