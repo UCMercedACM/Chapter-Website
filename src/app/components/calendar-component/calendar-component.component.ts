002b82bba53a7f96d559bc2664e1cda741b9aa47
Alberto <albertovalleflores99@gmail.com>
2020-04-15 21:35:01 -0700

    Revert "Implementing Calendar library (interactive calendar)"
    This reverts commit 3a5ed221d1e60e65b880a5c3c69525f87dccdb7d.

--- src/app/components/calendar-component/calendar-component.component.ts
+++ src/app/components/calendar-component/calendar-component.component.ts
@@ -1,93 +1,15 @@
-import { Component, 
-  OnInit,
-  ChangeDetectionStrategy,
-  ViewChild,
-  TemplateRef,
- } from '@angular/core';
-
- import {
-  startOfDay,
-  endOfDay,
-  subDays,
-  addDays,
-  endOfMonth,
-  isSameDay,
-  isSameMonth,
-  addHours,
-} from 'date-fns';
-import { Subject } from 'rxjs';
-
-import {
-  CalendarEvent,
-  CalendarEventAction,
-  CalendarEventTimesChangedEvent,
-  CalendarView,
-} from 'angular-calendar';
-
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3',
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF',
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA',
-  },
-};
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calendar-component',
-  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './calendar-component.component.html',
   styleUrls: ['./calendar-component.component.scss']
 })
-
 export class CalendarComponentComponent implements OnInit {
 
-  //Still need to add the modal
-  @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
+  constructor() { }
 
-  view: CalendarView = CalendarView.Month;
-
-  CalendarView = CalendarView;
-
-  viewDate: Date = new Date();
-
-  modalData: {
-    action: string;
-    event: CalendarEvent;
-  };
-
-  refresh: Subject<any> = new Subject();
-
-  activeDayIsOpen: boolean = true;
-
-  constructor() {}
-
-  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
-    if (isSameMonth(date, this.viewDate)) {
-      if (
-        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
-        events.length === 0
-      ) {
-        this.activeDayIsOpen = false;
-      } else {
-        this.activeDayIsOpen = true;
-      }
-      this.viewDate = date;
-    }
-  }
-  setView(view: CalendarView) {
-    this.view = view;
-  }
-
-  closeOpenMonthViewDay() {
-    this.activeDayIsOpen = false;
-  }
   ngOnInit() {
   }
+
 }
