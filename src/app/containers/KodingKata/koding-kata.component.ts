5381f9588bc37eb4614bef1168b03d24c157c0ca
Jatnael Montes <ifelsejet@gmail.com>
2020-04-26 21:19:04 -0700

    Redoing table

--- src/app/containers/KodingKata/koding-kata.component.ts
+++ src/app/containers/KodingKata/koding-kata.component.ts
@@ -1,12 +1,58 @@
 import { Component, OnInit } from "@angular/core";
 
+
 @Component({
   selector: "app-koding-kata",
   templateUrl: "./koding-kata.component.html",
   styleUrls: ["./koding-kata.component.scss"]
 })
 export class KodingKataComponent implements OnInit {
+  
+  headers = ["Rank", "Name", "Points"];
+  rows = [
+    {
+      "Rank": "1st",
+      "Name": "Paulo S.",
+      "Points": "500"
+    },
+    {
+      "Rank": "2nd",
+      "Name": "Sean C.",
+      "Points": "450"
+    },
+    {
+      "Rank": "3rd",
+      "Name": "Busher B.",
+      "Points": "300"
+    }
+  ]
+  
+  leaderboard: any[] = [
+    {
+      rank: "1st",
+      name: "Paulo S.",
+      points: "500"
+     
+    },
+    {
+     
+      rank: "2nd",
+      name: "Sean C.",
+      points: "450"
+    },
+    {
+      rank: "3rd", 
+      name: "Busher B",
+      points: "440"
+    },
+  ];
+
+  
+  
   constructor() {}
+
+  
 
   ngOnInit() {}
 }
+
