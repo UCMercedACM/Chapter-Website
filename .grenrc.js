72ea4e402f01ec1528864a73880fd86ef234a932
Adrian Darian <adarian@ucmerced.edu>
2020-06-03 01:10:01 -0700

    docs(release): updated release config and commiting

--- .grenrc.js
+++ .grenrc.js
@@ -1,37 +1,14 @@
 module.exports = {
-    dataSource: "prs",
-    prefix: "Release ",
-    ignoreLabels: [
-        "actions",
-        "angular",
-        "dependencies",
-        "documentation",
-        "duplicate",
-        "help wanted",
-        "invalid",
-        "question",
-        "test",
-        "wontfix",
-        "workflows",
-    ],
-    template: {
-        commit: ({ message, url, author, name }) =>
-            `- [${message}](${url}) - ${author ? `@${author}` : name}`,
-        issue: "- {{labels}} {{name}} [{{text}}]({{url}})",
-        label: "[**{{label}}**]",
-        noLabel: "closed",
-        group: "\n#### {{heading}}\n",
-        changelogTitle: "# Changelog\n\n",
-        release: function (placeholders, body) {
-            var fDate = new Date(placeholders.date);
-            return `## ${placeholders.release} ${fDate}\n${placeholders.body}`;
-        },
-        releaseSeparator: "\n---\n\n",
+  dataSource: "commits",
+  prefix: "Release ",
+  template: {
+    commit: ({ message, url, author, name }) =>
+      `- [${message}](${url}) - ${author ? `@${author}` : name}`,
+    release: function (placeholders, body) {
+      var fDate = new Date(placeholders.date);
+      return `## ${placeholders.release} ${fDate}\n${placeholders.body}`;
     },
-    onlyMilestones: false,
-    groupBy: {
-        "Features:": ["enhancement"],
-        "Bug Fixes:": ["bug"],
-    },
-    changelogFilename: "CHANGELOG.md",
+    releaseSeparator: "\n---\n\n",
+  },
+  onlyMilestones: false,
 };
