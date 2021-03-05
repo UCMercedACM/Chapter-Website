9e8fd40df8c367095b2dbae308253c1bc2298c75
Adrian Darian <adarian@ucmerced.edu>
2021-03-05 13:50:55 -0800

    add: initial setup for the 4 different main pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EventsComponent } from './pages/events/events.component';
+import { SigsComponent } from './pages/sigs/sigs.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EventsComponent,
+    SigsComponent,
+    ResourcesComponent,
   ],
   imports: [
     BrowserModule,
