dcde22015e12b88c89f45669a5c9a39f84796616
Adrian Darian <adarian@ucmerced.edu>
2021-03-16 20:13:17 -0700

    add: pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from "./pages/home/home.component";
 import { EventsComponent } from "./pages/events/events.component";
 import { SigsComponent } from "./pages/sigs/sigs.component";
 import { ResourcesComponent } from "./pages/resources/resources.component";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { ResourcesComponent } from "./pages/resources/resources.component";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
