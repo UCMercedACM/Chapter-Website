a6030bde43ac3da80a196307ea9d82a21f6fc7d0
Adrian Darian <adarian@ucmerced.edu>
2021-04-04 19:54:54 -0700

    add leadership profiles and fixed a few ui bugs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from "@angular/core";
 
+import leadership from "../../../assets/data/leadership.json";
+import { LeadershipInterface } from "../../../assets/interfaces/leadership.model";
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  public leadership: Array<LeadershipInterface> = leadership;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
