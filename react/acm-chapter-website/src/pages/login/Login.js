726a79a2dab154197f76e92864c4b667b35e61a4
Salih Saygi <salih.saygie@gmail.com>
2021-10-21 16:37:05 -0700

    Login Validation Done

--- react/acm-chapter-website/src/pages/login/Login.js
+++ react/acm-chapter-website/src/pages/login/Login.js
@@ -1,94 +1,91 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Login.scss";
-import { useAuth } from "../../contexts/AuthContext"
-import { useHistory } from "react-router-dom"
-import { useFormik } from 'formik';
+import { useAuth } from "../../contexts/AuthContext";
+import { useHistory } from "react-router";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import ErrorCard from "../../components/ErrorCard/ErrorCard";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
 
 const Login = () => {
-  const {login} = useAuth()
-
+  const { login } = useAuth();
   const history = useHistory();
- 
-  const validate = values => {
-    const errors = {};
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email address';
-    }
-    if (!values.password) {
-      errors.password = 'Required';
-    } 
-    return errors;
-  };
-
-
-
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validate,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-      login(values.email,values.password)
-      history.push("/");
-    },
-  });
-
   return (
-    <div>
-      <main className="login">
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={LoginSchema}
+      validateOnBlur
+      onSubmit={(values) => {
+        console.log(values);
 
-    <form onSubmit={formik.handleSubmit} className="login__landing">
-    <header>
-            <h2>Login</h2>
-            <p>Welcome to ACM @UCM</p>
-          </header>
-   
-    <input
-      className="input"
-      id="email"
-      placeholder="Email"
-      name="email"
-      type="email"
-      onChange={formik.handleChange}
-      value={formik.values.email}
-    />
-
-    <input
-      className="input"
-      id="password"
-      placeholder="Password"
-      name="password"
-      type="password"
-      onChange={formik.handleChange}
-      value={formik.values.password}
-    />
-
-    
-
-   
-
-    <div className="footer-buttons">
-            <div className="align-x">
-              <input id="c1d" type="checkbox" />
-              <label for="c1d">Remember me?</label>
-              <Link to="/reset">Forgot my password?</Link>
+        login(values.email, values.password);
+        history.push("/dashboard");
+      }}
+    >
+      {({ errors, handleSubmit, handleChange, values }) => (
+        <main className="login">
+          <Form onSubmit={handleSubmit} className="login__landing">
+            <header>
+              <h2>Login</h2>
+              <p>Welcome to ACM @UCM</p>
+            </header>
+            <div className="field">
+              <Field
+                className="input"
+                id="email"
+                placeholder="Email"
+                name="email"
+                type="email"
+                onChange={handleChange}
+                value={values.email}
+              />
+              <div className="error">
+                <ErrorMessage name="email" />
+              </div>
             </div>
-            <button type="submit" onClick={formik.handleSubmit}>
-              Sign In
-            </button>
-            <div className="align-x createAcc">
-              <p>Don't have an account?</p>
-              <Link to="/register">Create an account</Link>
+            <div className="field">
+              <Field
+                className="input"
+                id="password"
+                placeholder="Password"
+                name="password"
+                type="password"
+                onChange={handleChange}
+                value={values.password}
+              />
+              <div className="error">
+                <ErrorMessage name="password" />
+              </div>
             </div>
-          </div>
-  </form>
-      </main>
-    </div>
+            <div className="footer-buttons">
+              <div className="align-x">
+                <input id="c1d" type="checkbox" />
+                <label for="c1d">Remember me?</label>
+                <Link to="/reset">Forgot my password?</Link>
+              </div>
+              <button type="submit" onClick={handleSubmit}>
+                Sign In
+              </button>
+              <div className="align-x createAcc">
+                <p>Don't have an account?</p>
+                <Link to="/register">Create an account</Link>
+              </div>
+            </div>
+          </Form>
+        </main>
+      )}
+    </Formik>
   );
 };
 
