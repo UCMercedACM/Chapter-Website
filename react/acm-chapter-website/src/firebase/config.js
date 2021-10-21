3d0792518bbe91fd49d9f9f27b3db2baeda06bf1
Sean <seanoinca@gmail.com>
2021-10-20 20:56:13 -0700

    removed key

--- react/acm-chapter-website/src/firebase/config.js
+++ react/acm-chapter-website/src/firebase/config.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 const firebaseConfig = {
-  apiKey: "AIzaSyDQbTZu8aeGN7a10cqrsSaZxcUCF0hKor8",
-  authDomain: "acm-chapter-website.firebaseapp.com",
-  projectId: "acm-chapter-website",
-  storageBucket: "acm-chapter-website.appspot.com",
-  messagingSenderId: "371132582015",
-  appId: "1:371132582015:web:8e482dd5417a07252a574a"
+   
 };
 
 firebase.initializeApp(firebaseConfig);
