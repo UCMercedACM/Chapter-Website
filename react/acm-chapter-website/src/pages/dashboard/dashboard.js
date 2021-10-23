a05f42c40cceb21d2a7e1233f5ceedfc24d9d64b
Salih Saygi <salih.saygie@gmail.com>
2021-10-23 06:47:38 -0700

    Created a leadership components from Home Page to simplify

--- react/acm-chapter-website/src/pages/dashboard/dashboard.js
+++ react/acm-chapter-website/src/pages/dashboard/dashboard.js
@@ -1,38 +1,39 @@
-import React, { useState } from "react"
-import { Card, Button, Alert } from "react-bootstrap"
-import { useAuth } from "../../contexts/AuthContext"
-import { Link, useHistory } from "react-router-dom"
-import "../login/Login.scss"
+import React, { useState } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import "../login/Login.scss";
 export default function Dashboard() {
-  const [error, setError] = useState("")
-  const { currentUser, logout } = useAuth()
-  const history = useHistory()
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
 
   async function handleLogout() {
-    setError("")
+    setError("");
 
     try {
-      await logout()
-      history.push("/login")
+      await logout();
+      history.push("/login");
     } catch {
-      setError("Failed to log out")
+      setError("Failed to log out");
     }
   }
 
   return (
     <>
+      <main className="login">
+        <div className="login__landing">
+          <header>
+            <h2>Profile</h2>
+            <p>Welcome {currentUser.name}</p>
+          </header>
 
-<main className="login">
-<div  className="login__landing">
-<header>
-        <h2>Profile</h2>
-        <p>Welcome {currentUser.name}</p>
-      </header>
-
-
-<button onClick={handleLogout}type="logout">Logout</button>
-</div>
+          <button onClick={handleLogout} type="logout">
+            Logout
+          </button>
+          <p>{error}</p>
+        </div>
       </main>
     </>
-  )
+  );
 }
