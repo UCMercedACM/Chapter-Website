2dacd62e257ebed4556cd11e703b34acb7924df0
Salih Saygi <salih.saygie@gmail.com>
2021-10-23 06:45:23 -0700

    Register Validation Done

--- react/acm-chapter-website/src/pages/register/Register.js
+++ react/acm-chapter-website/src/pages/register/Register.js
@@ -1,107 +1,96 @@
 import React from "react";
 import "../login/Login.scss";
-import { useFormik } from "formik";
+import { ErrorMessage, Formik, Form, Field } from "formik";
 import { useAuth } from "../../contexts/AuthContext";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string()
+    .email("Invalid email")
+    .matches(
+      /([a-zA-Z0-9]+)([\.{1}])?([a-zA-Z0-9]+)\@ucmerced([\.])edu/g,
+      "Not @ucmerced.edu Email"
+    )
+    .required("Required"),
+  password: Yup.string()
+    .min(6, "Too Short!")
+    .max(15, "Too Long!")
+    .required("Required"),
+  confirmPassword: Yup.string()
+    .min(6, "Too Short!")
+    .max(15, "Too Long!")
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Required"),
+});
 
 const Register = () => {
-  const validate = (values) => {
-    const errors = {};
-    if (!values.name) {
-      errors.name = "Required";
-    } else if (values.name.length > 15) {
-      errors.name = "Must be 15 characters or less";
-    }
-    if (!values.email) {
-      errors.email = "Required";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Invalid email address";
-    }
-    if (!values.password) {
-      errors.password = "Required";
-    }
-    if (values.password !== values.confirmPassword) {
-      errors.confirmPassword = "Required";
-    }
-
-    return errors;
-  };
   const { signup } = useAuth();
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-    },
-    validate,
-    onSubmit: (values) => {
-      signup(values.email, values.password, values.name);
-    },
-  });
   return (
-    <div>
-      <main className="login">
-        <form onSubmit={formik.handleSubmit} className="login__landing">
-          <header>
-            <h2>Register</h2>
-            <p>Welcome to ACM @UCM</p>
-          </header>
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => {
+        signup(values.email, values.password, values.name);
+      }}
+    >
+      <div>
+        <main className="login">
+          <Form className="login__landing">
+            <header>
+              <h2>Register</h2>
+              <p>Welcome to ACM @UCM</p>
+            </header>
 
-          <input
-            className="input"
-            id="name"
-            name="name"
-            type="text"
-            placeholder="Name"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-          />
-          {formik.errors.name && formik.touched.name ? (
-            <div>{formik.errors.name}</div>
-          ) : null}
+            <Field
+              className="input"
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Name"
+            />
+            <ErrorMessage name="name" />
 
-          <input
-            className="input"
-            id="email"
-            placeholder="Email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-          {formik.errors.email && formik.touched.email ? (
-            <div>{formik.errors.email}</div>
-          ) : null}
-          <input
-            className="input"
-            id="password"
-            placeholder="Password"
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          />
-          {formik.errors.password && formik.touched.password ? (
-            <div>{formik.errors.password}</div>
-          ) : null}
-          <input
-            className="input"
-            id="confirmPassword"
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirm Password"
-            onChange={formik.handleChange}
-            value={formik.values.confirmPassword}
-          />
-          {formik.errors.confirmPassword && formik.touched.confirmPassword ? (
-            <div>{formik.errors.confirmPassword}</div>
-          ) : null}
-          <button type="submit">Submit</button>
-        </form>
-      </main>
-    </div>
+            <Field
+              className="input"
+              id="email"
+              placeholder="Email"
+              name="email"
+              type="email"
+            />
+            <ErrorMessage name="email" />
+
+            <Field
+              className="input"
+              id="password"
+              placeholder="Password"
+              name="password"
+              type="password"
+            />
+            <ErrorMessage name="password" />
+
+            <Field
+              className="input"
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              placeholder="Confirm Password"
+            />
+            <ErrorMessage name="confirmPassword" />
+
+            <button type="submit">Submit</button>
+          </Form>
+        </main>
+      </div>
+    </Formik>
   );
 };
 
