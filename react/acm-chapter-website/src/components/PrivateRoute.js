8e79be3f138bfde828b3ddc6e3c2e5e99c2bfd50
Salih Saygi <salih.saygie@gmail.com>
2021-10-25 20:21:40 -0700

    add auth check for private route

--- react/acm-chapter-website/src/components/PrivateRoute.js
+++ react/acm-chapter-website/src/components/PrivateRoute.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { auth } from "../firebase/config";
 
 export default function PrivateRoute({ component: Component, ...rest }) {
   const { currentUser } = useAuth();
+  const history = useHistory();
+  if (!currentUser) {
+    history.push("/login");
+  }
 
   return (
     <Route
