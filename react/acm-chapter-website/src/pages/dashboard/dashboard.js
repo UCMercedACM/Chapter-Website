d63d74ed25c2a218ef68599ef7335785ad2174c4
robokae <alexanderhom19@gmail.com>
2021-10-26 01:42:22 -0700

    initial dashboard page JSX and SCSS

--- react/acm-chapter-website/src/pages/dashboard/dashboard.js
+++ react/acm-chapter-website/src/pages/dashboard/dashboard.js
@@ -1,7 +1,74 @@
 import React, { useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
-import "../login/Login.scss";
+import "../dashboard/dashboard.scss";
+import userData from "./testUserData";
+
+const getTimeOfDay = () => {
+  const currDate = new Date();
+  let currHour = currDate.getHours();
+
+  if (currHour >= 0 && currHour < 12) {
+    return "morning";
+  }
+  else if (currHour >= 12 && currHour < 18) {
+    return "afternoon";
+  }
+  else {
+    return "evening";
+  }
+};
+
+const UserStats = () => {
+  return (
+    <div className="user-stats dashboard-component">
+      <div className="user-stats__left">
+        <h2 className="dashboard-component-heading">
+          Level {userData.levelInfo.level}
+        </h2>
+        <div className="user-stats__left__points-description">
+          <h1>{userData.levelInfo.points}/585</h1>
+        </div>
+        <p>
+          {userData.levelInfo.pointsUntilNextLevel} points until next level
+        </p>
+      </div>
+
+      <div className="user-stats__middle">
+        <h1>{userData.recentEvents.length}</h1>
+        <p>Events Attended</p>
+      </div>
+
+      <div className="user-stats__right">
+        <h1>{userData.levelInfo.leaderboardPosition}th</h1>
+        <p>In Leaderboard</p>
+      </div>
+    </div>
+  );
+};
+
+const UserProfile = () => {
+  return (
+    <div className="user-profile dashboard-component">
+      <h2 className="dashboard-component-heading">User Profile</h2>
+    </div>
+  );
+};
+
+const RecentEvents = () => {
+  return (
+    <div className="recent-events dashboard-component">
+      <h2 className="dashboard-component-heading">Events You Attended</h2>
+      <input type="text" name="eventCodeInput" id="eventCodeInput" placeholder="Enter event code" autoComplete="off" />
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Date</th>
+        </tr>
+      </table>
+    </div>
+  );
+};
 
 export default function Dashboard() {
   const [error, setError] = useState("");
@@ -20,10 +87,30 @@ export default function Dashboard() {
     }
   }
 
+  let timeOfDay = getTimeOfDay();
+  
   return (
     <>
-      <main className="login">
-        <div className="login__landing">
+      <main className="dashboard">
+        <section className="dashboard__content">
+          <h1 className="dashboard__content__heading">
+            Good {timeOfDay} {currentUser.name}
+          </h1>
+          <div className="dashboard__content__navbar">
+            <a href="#">Overview</a>
+            <a href="#">Settings</a>
+          </div>
+          <div className="dashboard__content__components">
+            <div className="dashboard__content__components__left">
+              <UserStats />
+              <RecentEvents />
+            </div>
+            <div className="dashboard__content__components__right">
+              <UserProfile />
+            </div>
+          </div>
+        </section>
+        {/* <div className="login__landing">
           <header>
             <h2>Profile</h2>
             <p>Welcome {currentUser.name}</p>
@@ -33,7 +120,7 @@ export default function Dashboard() {
             Logout
           </button>
           {error && <p>{error}</p>}
-        </div>
+        </div> */}
       </main>
     </>
   );
---
+++ react/acm-chapter-website/src/pages/dashboard/testUserData.js
@@ -0,0 +1,29 @@
+  // test data
+  const userData = {
+    levelInfo: {
+      level: 5,
+      points: 560,
+      pointsUntilNextLevel: 25,
+      leaderboardPosition: 5
+    },
+    recentEvents: [
+      {
+        name: "Loss Functions Workshop",
+        date: "10/14/2021"
+      },
+      {
+        name: "Firebase Workshop",
+        date: "10/19/2021"
+      },
+      {
+        name: "Koding Kata",
+        date: "10/20/2021"
+      }
+    ],
+    profile: {
+      joinDate: "October 28, 2021",
+      bio: "Lorem urna in ac cras sollicitudin integer. Feidn ekd ekdno if ejornk kejei ineput. Lorem urna in ac cras foene sollicitudin integer."
+    }
+  };
+
+  export default userData;
