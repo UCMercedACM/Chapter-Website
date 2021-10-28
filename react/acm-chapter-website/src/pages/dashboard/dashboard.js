740ddf23bb07a694e5d98c2fb9de37950e94dbac
robokae <alexanderhom19@gmail.com>
2021-10-28 01:00:08 -0700

    dashboard page JSX and styling

--- react/acm-chapter-website/src/pages/dashboard/dashboard.js
+++ react/acm-chapter-website/src/pages/dashboard/dashboard.js
@@ -21,25 +21,20 @@ const getTimeOfDay = () => {
 
 const UserStats = () => {
   return (
-    <div className="user-stats dashboard-component">
-      <div className="user-stats__left">
-        <h2 className="dashboard-component-heading">
-          Level {userData.levelInfo.level}
-        </h2>
-        <div className="user-stats__left__points-description">
-          <h1>{userData.levelInfo.points}/585</h1>
-        </div>
+    <div className="user-stats">
+      <div className="user-stats__left dashboard-component">
+        <h1>{userData.levelInfo.points} pts</h1>
         <p>
-          {userData.levelInfo.pointsUntilNextLevel} points until next level
+          Level {userData.levelInfo.level}
         </p>
       </div>
 
-      <div className="user-stats__middle">
+      <div className="user-stats__middle dashboard-component">
         <h1>{userData.recentEvents.length}</h1>
         <p>Events Attended</p>
       </div>
 
-      <div className="user-stats__right">
+      <div className="user-stats__right dashboard-component">
         <h1>{userData.levelInfo.leaderboardPosition}th</h1>
         <p>In Leaderboard</p>
       </div>
@@ -50,7 +45,23 @@ const UserStats = () => {
 const UserProfile = () => {
   return (
     <div className="user-profile dashboard-component">
-      <h2 className="dashboard-component-heading">User Profile</h2>
+      <section>
+        <div className="user-profile__profile-pic"></div>
+        <p className="user-profile__join-date">
+          Joined {userData.profile.joinDate}
+        </p>
+      </section>
+      <section>
+        <div className="user-profile__about-top">
+          <h3 className="user-profile__about-top__heading">About me</h3>
+          <button className="user-profile__about-top__edit-btn">Edit</button>
+        </div>
+        <div className="user-profile__about-bottom">
+          <p className="user-profile__about-bottom__bio">
+            {userData.profile.bio}
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
@@ -58,13 +69,21 @@ const UserProfile = () => {
 const RecentEvents = () => {
   return (
     <div className="recent-events dashboard-component">
-      <h2 className="dashboard-component-heading">Events You Attended</h2>
+      <h2 className="recent-events__heading dashboard-component-heading">Events You Attended</h2>
       <input type="text" name="eventCodeInput" id="eventCodeInput" placeholder="Enter event code" autoComplete="off" />
-      <table>
+      <table className="recent-events__table">
         <tr>
           <th>Name</th>
           <th>Date</th>
         </tr>
+        {userData.recentEvents.map((event) => {
+          return (
+            <tr>
+              <td>{event.name}</td>
+              <td>{event.date}</td>
+            </tr>
+          );
+        })}
       </table>
     </div>
   );
@@ -90,38 +109,45 @@ export default function Dashboard() {
   let timeOfDay = getTimeOfDay();
   
   return (
-    <>
-      <main className="dashboard">
-        <section className="dashboard__content">
-          <h1 className="dashboard__content__heading">
+    <main className="dashboard">
+      <section className="dashboard__content">
+        <div className="dashboard__content__top">
+          <h1 className="dashboard__content__top__heading">
             Good {timeOfDay} {currentUser.name}
           </h1>
-          <div className="dashboard__content__navbar">
-            <a href="#">Overview</a>
-            <a href="#">Settings</a>
-          </div>
-          <div className="dashboard__content__components">
-            <div className="dashboard__content__components__left">
-              <UserStats />
-              <RecentEvents />
-            </div>
-            <div className="dashboard__content__components__right">
-              <UserProfile />
-            </div>
-          </div>
-        </section>
-        {/* <div className="login__landing">
-          <header>
-            <h2>Profile</h2>
-            <p>Welcome {currentUser.name}</p>
-          </header>
-
-          <button onClick={handleLogout} type="logout">
-            Logout
+          <button 
+            onclick={handleLogout} 
+            type="logout" 
+            className="dashboard__content__top__log-out-btn"
+          >
+            Log Out
           </button>
-          {error && <p>{error}</p>}
+        </div>
+        {/* <div className="dashboard__content__top__navbar">
+          <a href="#">Overview</a>
+          <a href="#">Settings</a>
         </div> */}
-      </main>
-    </>
+        <div className="dashboard__content__components">
+          <div className="dashboard__content__components__left">
+            <UserStats />
+            <RecentEvents />
+          </div>
+          <div className="dashboard__content__components__right">
+            <UserProfile />
+          </div>
+        </div>
+      </section>
+      {/* <div className="login__landing">
+        <header>
+          <h2>Profile</h2>
+          <p>Welcome {currentUser.name}</p>
+        </header>
+
+        <button onClick={handleLogout} type="logout">
+          Logout
+        </button>
+        {error && <p>{error}</p>}
+      </div> */}
+    </main>
   );
 }
