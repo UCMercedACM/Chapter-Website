ae048058879ce0d992b40412f735c347c8ef2b2e
Salih Saygi <salih.saygie@gmail.com>
2022-02-13 15:19:19 -0800

    fixed #180

--- react/acm-chapter-website/src/firebase/config.js
+++ react/acm-chapter-website/src/firebase/config.js
@@ -1,16 +1,22 @@
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFNdbiYkCP20YXNqNfZAK0ERwJ9LNNJs8",
-  authDomain: "acm-chapter-website.firebaseapp.com",
-  projectId: "acm-chapter-website",
-  storageBucket: "acm-chapter-website.appspot.com",
-  messagingSenderId: "371132582015",
-  appId: "1:371132582015:web:fc2a4ce7b1c324462a574a",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6LcAFHgeAAAAANy5llS2hE-lKe1SIUxppUIaJWLF"),
+  isTokenAutoRefreshEnabled: true,
+});
 export const db = getFirestore(app);
-export const auth = getAuth();
+export const auth = getAuth(app);
