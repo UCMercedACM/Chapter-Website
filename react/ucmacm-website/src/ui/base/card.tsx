1620dec93a37278ef4c67588345c601639cabc12
No767 <73260931+No767@users.noreply.github.com>
2023-11-27 17:36:36 -0800

    Add base card

--- react/ucmacm-website/src/ui/base/card.tsx
+++ react/ucmacm-website/src/ui/base/card.tsx
@@ -1,11 +1,19 @@
+interface CardProps {
+  description: string;
+}
 
-const Card = () => {
+const Card = ({ description }: CardProps) => {
   return (
-    <div className="h-20 container">
-      <h1 className="decoration-white">header</h1>
-      <h1 className="bg-white h-20 container">hi</h1>
-      <p>hello world</p>
-    </div>
+    // <div className="container 2xl bg-slate-100 rounded-xl">
+    //   <p>hello world</p>
+    // </div>
+    <div className="drop-shadow-md">
+    <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
+
+{/* <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5> */}
+<p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
+</div>
+</div>
   );
 }
 
