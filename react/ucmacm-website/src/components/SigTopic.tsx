301cd0e5603fe79c6a661ee858c6a547be1994ad
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2024-03-05 00:16:30 -0800

    Add SIGS page (#305)
    Co-authored-by: No767 <73260931+No767@users.noreply.github.com>
    Co-authored-by: Trek <trek@Treks-MacBook-Air.local>

---
+++ react/ucmacm-website/src/components/SigTopic.tsx
@@ -0,0 +1,60 @@
+import { SectionTitle } from './SectionTitle'
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableColumn,
+    TableRow,
+    TableCell
+  } from "@nextui-org/react";
+
+interface Event {
+    title: string,
+    date: string,
+    time: string,
+    description: string,
+    location: string
+}
+
+interface SigTopicProps {
+    title: string,
+    description: string,
+    events: Event[]
+}
+
+const SigTopic = ({ title, description, events}: SigTopicProps) => {
+  return (
+    <>
+        <SectionTitle title={title} />
+        <p className="">
+            {description}
+        </p>
+        <h3 className="text-tertiary font-bold mt-8">UPCOMING EVENTS:</h3>
+        <div className="mx-4 my-8">
+            <Table 
+                aria-label="SIG Event list" 
+                fullWidth={false}
+            >
+                <TableHeader>
+                    <TableColumn>Date & Time</TableColumn>
+                    <TableColumn>Event</TableColumn>
+                    <TableColumn>Location</TableColumn>
+                </TableHeader>
+                <TableBody>
+                    {
+                        events.map((event) => (
+                        <TableRow key={event.title}>
+                            <TableCell>{event.date} - {event.time}</TableCell>
+                            <TableCell>{event.title}</TableCell>
+                            <TableCell>{event.location}</TableCell>
+                        </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </div>
+    </>
+  )
+}
+
+export default SigTopic
--- react/ucmacm-website/src/pages/front.tsx
+++ react/ucmacm-website/src/pages/front.tsx
@@ -1,24 +1,29 @@
 import GlobalNavbar from "../components/GlobalNavbar";
 import GlobalFooter from "../components/GlobalFooter";
-import WaveHeader from "../components/WaveHeader";
 import AboutUsImage from "../assets/images/about-us.png";
-import SocialResponsibility from "../assets/images/social-responsibility.png";
-import ProjectManagement from "../assets/images/project-management.png";
-import OnlineCourses from "../assets/images/online-course.png";
-import Communities from "../assets/images/communities.png";
-
 import "../index.css";
 function Front() {
   return (
     <>
       <GlobalNavbar />
-      <WaveHeader />
+      <div className="bg-landing opacity-80 py-8 w-full max-h-max top-16 gap-16 flex-col flex">
+        <p>hi</p>
+      </div>
+      <div className="z-0 gap-16 flex">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#00e1bf"
+            fillOpacity="0.8"
+            d="M0,256L80,224C160,192,320,128,480,122.7C640,117,800,171,960,192C1120,213,1280,203,1360,197.3L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
 
       <section className="mx-24">
         <h1 className="font-bold text-center text-4xl text-tertiary pb-16">
           About Us
         </h1>
-        <div className="columns-2 items-center">
+        <div className="columns-2 gap-x-24">
           <div className="container-2xl">
             <p className="text-tertiary text-xl">
               Association for Computing Machinery (ACM) is the biggest computing
@@ -46,59 +51,21 @@ function Front() {
         <h1 className="font-bold text-center text-4xl text-tertiary pt-16 pb-16">
           Goals
         </h1>
+        <div className="columns-2 grid-rows-2 gap-x-24">
+          <div className="container-2xl">
+            <div>
+              <p className="text-tertiary text-xl">yes</p>
+            </div>
+            <div>
+              <p className="text-tertiary text-xl">yes</p>
+            </div>
 
-        {/*TEST THIS AGAINST MOBILE UI - Noelle*/}
-        <div className="grid-rows-2 grid-cols-2 md:columns-1 gap-y-8 justify-center flex flex-row gap-x-24 text-tertiary">
-          <div className="flex flex-col items-center text-center text-wrap max-w-md">
-            <img
-              className="py-2"
-              src={ProjectManagement}
-              width="100"
-              height="100"
-            />
-            <p className="font-semibold text-xl pb-4">Technical Excellence</p>
-            <p className="text-md">
-              Preparing the club and its members for work in the industry, at
-              the highest quality of efficiency and scalability.
-            </p>
-          </div>
-          <div className="flex flex-col items-center text-center text-wrap max-w-md">
-            <img
-              className="py-2"
-              src={OnlineCourses}
-              width="100"
-              height="100"
-            />
-            <p className="font-semibold text-xl pb-4">
-              Educational Transparency
-            </p>
-            <p className="text-md">
-              Allowing ease of access for all academic topics related to
-              computer science and in our course materials.
-            </p>
-          </div>
-          <div className="flex flex-col items-center text-center text-wrap max-w-md">
-            <img className="py-2" src={Communities} width="100" height="100" />
-            <p className="font-semibold text-xl pb-4">Community</p>
-            <p className="text-md">
-              Providing a welcoming environment for computer scientists from all
-              areas of technical knowledge and tying that back to a sense of
-              campus culture.
-            </p>
-          </div>
-          <div className="flex flex-col items-center text-center text-wrap max-w-md">
-            <img
-              className="py-2"
-              src={SocialResponsibility}
-              width="100"
-              height="100"
-            />
-            <p className="font-semibold text-xl pb-4">Social Responsibility</p>
-            <p className="text-md">
-              Ensuring that our members are involved in social and corporate
-              environments that uphold high standards of ethical and moral
-              responsibility.
-            </p>
+            <div>
+              <p className="text-tertiary text-xl">yes</p>
+            </div>
+            <div>
+              <p className="text-tertiary text-xl">yes</p>
+            </div>
           </div>
         </div>
       </section>
