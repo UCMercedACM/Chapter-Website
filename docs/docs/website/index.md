c8e53464dcd77b482fb522f6a8c751938f4d00dc
No767 <73260931+No767@users.noreply.github.com>
2024-09-05 00:16:15 -0700

    rewrite docs

---
+++ docs/docs/website/index.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 2
+slug: /website
+---
+
+# Website Documentation
+
+This page serves the website project's documentation. Look at the sidebar for the pages.
--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -35,26 +35,28 @@ const config: Config = {
 			{
 				docs: {
 					sidebarPath: "./sidebars.ts",
+					routeBasePath: "/",
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
 						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
 				},
-				blog: {
-					showReadingTime: true,
-					feedOptions: {
-						type: ["rss", "atom"],
-						xslt: true,
-					},
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-					// Useful options to enforce blogging best practices
-					onInlineTags: "warn",
-					onInlineAuthors: "warn",
-					onUntruncatedBlogPosts: "warn",
-				},
+				blog: false,
+				// {
+				// showReadingTime: true,
+				// feedOptions: {
+				// 	type: ["rss", "atom"],
+				// 	xslt: true,
+				// },
+				// // Please change this to your repo.
+				// // Remove this to remove the "edit this page" links.
+				// editUrl:
+				// 	"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+				// // Useful options to enforce blogging best practices
+				// onInlineTags: "warn",
+				// onInlineAuthors: "warn",
+				// onUntruncatedBlogPosts: "warn",
+				// },
 				theme: {
 					customCss: "./src/css/custom.css",
 				},
@@ -71,65 +73,52 @@ const config: Config = {
 				alt: "My Site Logo",
 				src: "img/logo.svg",
 			},
-			items: [
-				{
-					type: "docSidebar",
-					sidebarId: "tutorialSidebar",
-					position: "left",
-					label: "Tutorial",
-				},
-				{ to: "/blog", label: "Blog", position: "left" },
-				{
-					href: "https://github.com/facebook/docusaurus",
-					label: "GitHub",
-					position: "right",
-				},
-			],
+			items: [],
 		},
 		footer: {
 			style: "dark",
 			links: [
-				{
-					title: "Docs",
-					items: [
-						{
-							label: "Tutorial",
-							to: "/docs/intro",
-						},
-					],
-				},
-				{
-					title: "Community",
-					items: [
-						{
-							label: "Stack Overflow",
-							href: "https://stackoverflow.com/questions/tagged/docusaurus",
-						},
-						{
-							label: "Discord",
-							href: "https://discordapp.com/invite/docusaurus",
-						},
-						{
-							label: "Twitter",
-							href: "https://twitter.com/docusaurus",
-						},
-					],
-				},
-				{
-					title: "More",
-					items: [
-						{
-							label: "Blog",
-							to: "/blog",
-						},
-						{
-							label: "GitHub",
-							href: "https://github.com/facebook/docusaurus",
-						},
-					],
-				},
+				// {
+				// 	title: "Docs",
+				// 	items: [
+				// 		{
+				// 			label: "Tutorial",
+				// 			to: "/docs/intro",
+				// 		},
+				// 	],
+				// },
+				// {
+				// 	title: "Community",
+				// 	items: [
+				// 		{
+				// 			label: "Stack Overflow",
+				// 			href: "https://stackoverflow.com/questions/tagged/docusaurus",
+				// 		},
+				// 		{
+				// 			label: "Discord",
+				// 			href: "https://discordapp.com/invite/docusaurus",
+				// 		},
+				// 		{
+				// 			label: "Twitter",
+				// 			href: "https://twitter.com/docusaurus",
+				// 		},
+				// 	],
+				// },
+				// {
+				// 	title: "More",
+				// 	items: [
+				// 		{
+				// 			label: "Blog",
+				// 			to: "/blog",
+				// 		},
+				// 		{
+				// 			label: "GitHub",
+				// 			href: "https://github.com/facebook/docusaurus",
+				// 		},
+				// 	],
+				// },
 			],
-			copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+			copyright: `Copyright © ${new Date().getFullYear()} ACM @ UC Merced. Built with Docusaurus.`,
 		},
 		prism: {
 			theme: prismThemes.github,
