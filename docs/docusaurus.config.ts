553be5883ff1beb405edfd50073a9545615d8035
No767 <73260931+No767@users.noreply.github.com>
2024-09-05 00:23:33 -0700

    Finish

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,20 +3,20 @@ import type { Config } from "@docusaurus/types";
 import { themes as prismThemes } from "prism-react-renderer";
 
 const config: Config = {
-	title: "My Site",
+	title: "ACM @ UC Merced Documentation",
 	tagline: "Dinosaurs are cool",
-	favicon: "img/favicon.ico",
+	favicon: "img/acm_logo_32.png",
 
 	// Set the production url of your site here
-	url: "https://your-docusaurus-site.example.com",
+	url: "https://ucmacm-docs.netlify.app",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
+	organizationName: "UCMercedACM", // Usually your GitHub org/user name.
+	projectName: "docs", // Usually your repo name.
 
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
@@ -39,7 +39,7 @@ const config: Config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+						"https://github.com/UCMercedACM/Chapter-Website/tree/react-rewrite/docs/docs",
 				},
 				blog: false,
 				// {
@@ -66,12 +66,12 @@ const config: Config = {
 
 	themeConfig: {
 		// Replace with your project's social card
-		image: "img/docusaurus-social-card.jpg",
+		image: "img/acm_logo.png",
 		navbar: {
-			title: "My Site",
+			title: "ACM @ UC Merced Documentation",
 			logo: {
-				alt: "My Site Logo",
-				src: "img/logo.svg",
+				alt: "ACM @ UC Merced Logo",
+				src: "img/acm_logo.png",
 			},
 			items: [],
 		},
