63c4a4bbaf0c7198ea71bed8aa8c0d0c0ab8c924
Noelle Wang <73260931+No767@users.noreply.github.com>
2024-12-14 22:37:54 -0800

    Fix broken dependencies (#582)

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const { nextui } = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
 export default {
 	content: [
 		"./index.html",
