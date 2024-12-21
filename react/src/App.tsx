22273c87cf88af2b4847c74675902603c0066f54
No767 <73260931+No767@users.noreply.github.com>
2024-12-21 00:33:23 -0800

    Get oauth2 working

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,14 +1,49 @@
 import { Route, Routes } from "react-router-dom";
+import * as reactRouterDom from "react-router-dom";
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import Passwordless from "supertokens-auth-react/recipe/passwordless";
+import { PasswordlessPreBuiltUI } from "supertokens-auth-react/recipe/passwordless/prebuiltui";
+import Session from "supertokens-auth-react/recipe/session";
+import ThirdParty from "supertokens-auth-react/recipe/thirdparty";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
 import Front from "./pages/front.tsx";
 import Sigs from "./pages/sigs.tsx";
+
+SuperTokens.init({
+	appInfo: {
+		appName: "ucmacm-website",
+		apiDomain: import.meta.env.VITE_API_DOMAIN,
+		websiteDomain: import.meta.env.VITE_WEBSITE_DOMAIN,
+		apiBasePath: "/api-auth",
+		websiteBasePath: "/auth",
+	},
+	recipeList: [
+		Passwordless.init({
+			contactMethod: "EMAIL",
+		}),
+		ThirdParty.init({
+			signInAndUpFeature: {
+				providers: [ThirdParty.Github.init(), ThirdParty.Google.init()],
+			},
+		}),
+		Session.init(),
+	],
+});
+
 function App() {
 	return (
 		<>
-			<Routes>
-				{/* public routes */}
-				<Route path="/" element={<Front />} />
-				<Route path="/sigs" element={<Sigs />} />
-			</Routes>
+			<SuperTokensWrapper>
+				<Routes>
+					{getSuperTokensRoutesForReactRouterDom(reactRouterDom, [
+						ThirdPartyPreBuiltUI,
+						PasswordlessPreBuiltUI,
+					])}
+					<Route path="/" element={<Front />} />
+					<Route path="/sigs" element={<Sigs />} />
+				</Routes>
+			</SuperTokensWrapper>
 		</>
 	);
 }
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -6,14 +6,17 @@ import "./index.css";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter } from "react-router-dom";
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
 
 // biome-ignore lint/style/noNonNullAssertion: Provided by React. Nothing we can do
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
+		{/* <SuperTokensWrapper> */}
 		<NextUIProvider>
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 		</NextUIProvider>
+		{/* </SuperTokensWrapper> */}
 	</React.StrictMode>,
 );
