f0898490318281d482fe06a930f7a2b5b87edfa7
No767 <73260931+No767@users.noreply.github.com>
2024-12-23 01:29:02 -0800

    move folders

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App.tsx'
+import './index.css'
+
+import { NextUIProvider } from '@nextui-org/react'
+import { BrowserRouter } from 'react-router-dom'
+
+// biome-ignore lint/style/noNonNullAssertion: Provided by React. Nothing we can do
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </NextUIProvider>
+  </React.StrictMode>,
+)
