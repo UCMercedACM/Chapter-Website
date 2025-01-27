238bc2134604a89307f4f2fb52746d2ecb2e7766
No767 <73260931+No767@users.noreply.github.com>
2025-01-26 23:55:14 -0800

    dupe

---
+++ react-old/src/pages/components/WaveHeader.tsx
@@ -0,0 +1,25 @@
+function WaveHeader() {
+	return (
+		<>
+			<div className="flex top-16 flex-col gap-16 py-8 w-full opacity-80 bg-landing max-h-max">
+				<p>hi</p>
+			</div>
+			<div className="flex z-0 gap-16">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 1440 320"
+					role="img"
+					aria-label="title"
+				>
+					<path
+						fill="#00e1bf"
+						fillOpacity="0.8"
+						d="M0,256L80,224C160,192,320,128,480,122.7C640,117,800,171,960,192C1120,213,1280,203,1360,197.3L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+					/>
+				</svg>
+			</div>
+		</>
+	);
+}
+
+export default WaveHeader;
