8e6b19eb29e85d0bb29953a2af0c7549aa85e477
No767 <73260931+No767@users.noreply.github.com>
2025-02-15 13:51:49 -0800

    Add react-scan on dev for performance issues

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { scan } from "react-scan";
 
 import App from "./App.tsx";
 import "./index.css";
 
 import { HeroUIProvider } from "@heroui/react";
 import { BrowserRouter } from "react-router";
+
+scan({
+	enabled: !import.meta.env.PROD,
+});
 
 // biome-ignore lint/style/noNonNullAssertion: Provided by React. Nothing we can do
 ReactDOM.createRoot(document.getElementById("root")!).render(
