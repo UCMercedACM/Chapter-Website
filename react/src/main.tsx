ecccfd592d546860c6ba2d5a3066877b42203721
Noelle Wang <73260931+No767@users.noreply.github.com>
2025-02-16 11:06:31 -0800

    Format suppression (#614)

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,3 +1,6 @@
+// biome-ignore format: supress import sorting
+import { scan } from "react-scan";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -6,6 +9,8 @@ import "./index.css";
 
 import { HeroUIProvider } from "@heroui/react";
 import { BrowserRouter } from "react-router";
+
+scan({ enabled: import.meta.env.DEV });
 
 // biome-ignore lint/style/noNonNullAssertion: Provided by React. Nothing we can do
 ReactDOM.createRoot(document.getElementById("root")!).render(
