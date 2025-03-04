196754fff35dcccecd29046f36e03852a50ee7fc
No767 <73260931+No767@users.noreply.github.com>
2025-03-03 18:20:49 -0800

    make the switch to shadcn

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -2,7 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
+import path from "node:path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
