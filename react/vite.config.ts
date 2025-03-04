ab10d05d0ede5bc265142c8cfde20901a7e3ad67
No767 <73260931+No767@users.noreply.github.com>
2025-03-03 18:22:35 -0800

    import sort

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,8 +1,8 @@
+import path from "node:path";
+
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-
-import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
