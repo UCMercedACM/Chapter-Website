5b7435b2e97c418bd501a30f21dc8b475126ea9c
No767 <73260931+No767@users.noreply.github.com>
2025-05-22 01:19:49 +0900

    .

--- react/src/routes/index/body.tsx
+++ react/src/routes/index/body.tsx
@@ -1,7 +1,7 @@
 import { FrontBanner } from "@/routes/index/banners";
 
 function Body() {
-	return <FrontBanner />;
+	return <div className="bg-[url(banners/mesh-banner-1.png)]" />;
 }
 
 export { Body };
--- react/src/routes/index/index.tsx
+++ react/src/routes/index/index.tsx
@@ -1,7 +1,13 @@
 import { Navbar } from "@/components/app/navbar";
+import { Body } from "@/routes/index/body";
 
 function Index() {
-	return <Navbar />;
+	return (
+		<>
+			<Navbar />
+			<Body />
+		</>
+	);
 }
 
 export { Index };
