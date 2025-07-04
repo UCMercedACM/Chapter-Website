8e19f5ed5d3b8e87f4406a3b3e9051c85e73dfd5
No767 <73260931+No767@users.noreply.github.com>
2025-07-05 01:08:27 +0900

    mostly finish front landing section

--- react/src/components/ui/button.tsx
+++ react/src/components/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        xl: "h-14 rounded-lg px-28 py-2 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
--- react/src/routes/index.tsx
+++ react/src/routes/index.tsx
@@ -1,7 +1,42 @@
+import { Link } from "react-router";
 import { Navbar } from "@/components/app/navbar";
+import { Button } from "@/components/ui/button";
 
 function Index() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+
+      <section className="container py-16 mx-auto md:py-28 px-18">
+        <div className="flex flex-col items-center md:flex-row">
+          <div className="mb-12 font-bold leading-8 text-center md:mb-0 md:w-1/2 lg:w-2/5">
+            <h2 className="mb-6 text-5xl">Where Theory and Practice Fuse</h2>
+            <p className="mb-6 text-lg">
+              Central Valley's Flagship Computer Science Community at UC Merced
+            </p>
+            <Button
+              variant="default"
+              size="xl"
+              className="text-lg font-bold outline-black bg-primary-alt hover:bg-primary-alt/90 hover:outline-solid"
+              asChild
+            >
+              <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                Join Today!
+              </Link>
+            </Button>
+          </div>
+
+          <div className="md:pl-12 md:w-1/2 lg:pl-20 lg:w-3/5">
+            <img
+              src="https://placehold.co/500x250/6366f1/ffffff?text=placeholder"
+              alt="placeholder"
+              className="w-full rounded-md shadow-2xl"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export { Index };
