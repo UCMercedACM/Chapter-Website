146c9c6e6d33d35254b2689c725f8b4b0b5852f0
No767 <73260931+No767@users.noreply.github.com>
2025-07-05 01:27:12 +0900

    disable auth for now

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,44 +1,45 @@
-// import * as reactRouterDom from "react-router";
+import * as reactRouter from "react-router";
 import { Route, Routes } from "react-router";
-// import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
-// import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
-// import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
-// import Session from "supertokens-auth-react/recipe/session";
-// import ThirdParty from "supertokens-auth-react/recipe/thirdparty";
-// import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
-// import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
+import Session from "supertokens-auth-react/recipe/session";
+import ThirdParty from "supertokens-auth-react/recipe/thirdparty";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
 import { Index } from "@/routes/index";
 
 function App() {
-  // SuperTokens.init({
-  //   appInfo: {
-  //     appName: "ucmacm-website",
-  //     apiDomain: import.meta.env.VITE_API_DOMAIN,
-  //     websiteDomain: import.meta.env.VITE_WEBSITE_DOMAIN,
-  //     apiBasePath: "/auth",
-  //     websiteBasePath: "/auth",
-  //   },
-  //   recipeList: [
-  //     EmailPassword.init(),
-  //     ThirdParty.init({
-  //       signInAndUpFeature: {
-  //         providers: [ThirdParty.Google.init()],
-  //       },
-  //     }),
-  //     Session.init(),
-  //   ],
-  // });
+  SuperTokens.init({
+    appInfo: {
+      appName: "ucmacm-website",
+      apiDomain: import.meta.env.VITE_API_DOMAIN,
+      websiteDomain: import.meta.env.VITE_WEBSITE_DOMAIN,
+      apiBasePath: "/auth",
+      websiteBasePath: "/auth",
+    },
+    recipeList: [
+      EmailPassword.init(),
+      ThirdParty.init({
+        signInAndUpFeature: {
+          providers: [ThirdParty.Google.init()],
+        },
+      }),
+      Session.init(),
+    ],
+    disableAuthRoute: false,
+  });
 
   return (
-    // <SuperTokensWrapper>
-    <Routes>
-      {/* {getSuperTokensRoutesForReactRouterDom(reactRouterDom, [
+    <SuperTokensWrapper>
+      <Routes>
+        {getSuperTokensRoutesForReactRouterDom(reactRouter, [
           ThirdPartyPreBuiltUI,
           EmailPasswordPreBuiltUI,
-        ])} */}
-      <Route path="/" element={<Index />} />
-    </Routes>
-    // </SuperTokensWrapper>
+        ])}
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </SuperTokensWrapper>
   );
 }
 
