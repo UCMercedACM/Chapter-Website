bc4b8f64c6f431eca6b2ae2d2df7c6c01fa1b7f6
No767 <73260931+No767@users.noreply.github.com>
2025-07-07 00:01:43 +0900

    sort imports better

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,5 +1,6 @@
 import * as reactRouter from "react-router";
 import { Route, Routes } from "react-router";
+
 import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
 import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
 import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
@@ -7,6 +8,7 @@ import Session from "supertokens-auth-react/recipe/session";
 import ThirdParty from "supertokens-auth-react/recipe/thirdparty";
 import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
 import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
+
 import { Index } from "@/routes/index";
 
 function App() {
