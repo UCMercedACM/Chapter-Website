f7ac871e3736b0773f14067663f98e89c7f99eec
No767 <73260931+No767@users.noreply.github.com>
2026-04-05 16:12:15 -0700

    done

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { Index } from "@/routes/index";
-
-function App() {
-  return <Index />;
-}
-
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import path from "node:path";
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+        tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
